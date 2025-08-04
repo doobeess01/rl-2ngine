c4cb8c19dfd8b31b88799d786d6603162e7d5520
David Dee <david.m.dee01@gmail.com>
2025-08-04 18:04:56 -0500

    Implemented adding all actors on the level to queue

--- game/world_tools.py
+++ game/world_tools.py
@@ -4,6 +4,7 @@ import tcod.ecs
 import g
 
 from game.components import Position, Graphic, Tiles, MapShape
+from game.tags import IsActor
 from game.queue import Queue
 from game.procgen import generate_map
 
@@ -17,4 +18,12 @@ def world_init():
     g.player = g.registry.new_entity(components={Position: Position(5,5, map_), Graphic: Graphic(ord('@'), (255,255,255))})
     
     queue = g.registry[None].components[Queue] = Queue()
-    queue.add(g.player)
+
+    enter_level(map_)
+
+
+def enter_level(map_: tcod.ecs.Entity):
+    g.queue().clear()
+    for e in [e for e in g.registry.Q.all_of(tags=[IsActor]) if e.components[Position].map_ == map_ and e != g.player]:
+        g.queue().add(e)
+    g.queue().add(g.player)  # Every other entity gets a turn before the player does
